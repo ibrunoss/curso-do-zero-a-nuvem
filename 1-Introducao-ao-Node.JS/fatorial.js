727f0addfcfaf36ea8996b43537451e1091642f4
Bruno Silva Santana <ibrunoss@outlook.com>
2021-07-19 14:52:36 -0300

    Usando Módulos Personalizados

---
+++ 1-Introducao-ao-Node.JS/fatorial.js
@@ -0,0 +1,17 @@
+const fatorial = (num) => {
+  if (num === 0) {
+    return 1;
+  }
+
+  return num * fatorial(num - 1);
+}
+
+//exports.fatorial = fatorial
+/* 
+ * Fazendo exportação da maneira acima estamos exportando todos os módulos junto 
+ * com o conteúdo do arquivo pois exports é uma referencia a module.exports
+ * Sendo assim para importar a fn fatorial é necessário fazer require("./fatorial").fatorial
+ */
+
+module.exports = fatorial
+//Usando Módulos Personalizados
--- 1-Introducao-ao-Node.JS/primeiro-script.js
+++ 1-Introducao-ao-Node.JS/primeiro-script.js
@@ -1,4 +1,6 @@
-console.log("n-fatorial");
+const fatorial = require("./fatorial")
+
+console.log("n-fatorial")
 
 console.log(`Executando o script a partir do diretório ${process.cwd()}`)
 
@@ -7,13 +9,5 @@ console.log("Argumentos passados na chamada", process.argv)
 process.on("exit", () => console.log("script está prestes a terminar"))
 
 const num = parseInt(process.argv[2])
-
-const fatorial = (num) => {
-  if (num === 0) {
-    return 1;
-  }
-
-  return num * fatorial(num - 1);
-}
 
 console.log(`O fatorial de ${num} é igual a ${fatorial(num)}`)
