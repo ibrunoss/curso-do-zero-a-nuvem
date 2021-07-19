dac13d59223ddda999469867ff80587498697e9d
Bruno Silva Santana <ibrunoss@outlook.com>
2021-07-19 14:55:57 -0300

    Excluindo exemplos de require

--- 1-Introducao-ao-Node.JS/fatorial.js
+++ 1-Introducao-ao-Node.JS/fatorial.js
@@ -1,5 +1,3 @@
-console.log(module.id)
-
 const fatorial = (num) => {
   if (num === 0) {
     return 1;
@@ -8,12 +6,4 @@ const fatorial = (num) => {
   return num * fatorial(num - 1);
 }
 
-//exports.fatorial = fatorial
-/* 
- * Fazendo exportação da maneira acima estamos exportando todos os módulos junto 
- * com o conteúdo do arquivo pois exports é uma referencia a module.exports
- * Sendo assim para importar a fn fatorial é necessário fazer require("./fatorial").fatorial
- */
-
 module.exports = fatorial
-//Usando Módulos Personalizados
