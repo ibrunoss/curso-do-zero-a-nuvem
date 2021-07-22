d2af8e2080b42663010cd627f8aafabe427e591f
Bruno Silva Santana <ibrunoss@outlook.com>
2021-07-22 16:33:59 -0300

    Primeiro Exemplo com Restify

--- 2-API-REST-Node-Restify/dist/main.js
+++ 2-API-REST-Node-Restify/dist/main.js
@@ -1,1 +1,13 @@
-console.log("main script...");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const PORT = 3000;
+const server = restify.createServer({
+    name: "meat-api",
+    version: "1.0.0",
+});
+server.get("/hello", (req, resp, next) => {
+    resp.json({ message: "Hello Word!" });
+    return next();
+});
+server.listen(PORT, () => console.log(`API is running on http://localhost:${PORT}`));
--- 2-API-REST-Node-Restify/main.ts
+++ 2-API-REST-Node-Restify/main.ts
@@ -1,1 +1,17 @@
-console.log("main script...");
+import * as restify from "restify";
+
+const PORT: number = 3000;
+
+const server = restify.createServer({
+  name: "meat-api",
+  version: "1.0.0",
+});
+
+server.get("/hello", (req, resp, next) => {
+  resp.json({ message: "Hello Word!" });
+  return next();
+});
+
+server.listen(PORT, () =>
+  console.log(`API is running on http://localhost:${PORT}`)
+);
