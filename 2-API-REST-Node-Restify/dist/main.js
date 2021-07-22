9e8615245d0d3bd913af18effa2a811ef72ea9ca
Bruno Silva Santana <ibrunoss@outlook.com>
2021-07-22 18:34:41 -0300

    Os Objetos Request, Response e Next

--- 2-API-REST-Node-Restify/dist/main.js
+++ 2-API-REST-Node-Restify/dist/main.js
@@ -6,8 +6,34 @@ const server = restify.createServer({
     name: "meat-api",
     version: "1.0.0",
 });
-server.get("/hello", (req, resp, next) => {
-    resp.json({ message: "Hello Word!" });
-    return next();
-});
+server.use(restify.plugins.queryParser());
+server.get("/info", [
+    (req, resp, next) => {
+        if (req.userAgent() && req.userAgent().includes("MSIE 7.0")) {
+            //resp.status(400);
+            //resp.json({ message: "Please, update your browser" });
+            //return next(false);
+            let error = new Error();
+            error.statusCode = 500;
+            error.message = "Please, update your browser";
+            return next(error);
+        }
+        return next();
+    },
+    (req, resp, next) => {
+        //resp.status(200);
+        //resp.statusMessage = "Ola mundo";
+        //resp.setHeader("Content-Type", "application/jason");
+        //resp.contentType = "application/jason";
+        //resp.send({ message: "Hello Word!" });
+        resp.json({
+            browser: req.userAgent(),
+            method: req.method,
+            url: req.href(),
+            path: req.path(),
+            query: req.query,
+        });
+        return next();
+    },
+]);
 server.listen(PORT, () => console.log(`API is running on http://localhost:${PORT}`));
--- 2-API-REST-Node-Restify/main.ts
+++ 2-API-REST-Node-Restify/main.ts
@@ -7,10 +7,40 @@ const server = restify.createServer({
   version: "1.0.0",
 });
 
-server.get("/hello", (req, resp, next) => {
-  resp.json({ message: "Hello Word!" });
-  return next();
-});
+server.use(restify.plugins.queryParser());
+
+server.get("/info", [
+  (req, resp, next) => {
+    if (req.userAgent() && req.userAgent().includes("MSIE 7.0")) {
+      //resp.status(400);
+      //resp.json({ message: "Please, update your browser" });
+      //return next(false);
+
+      let error: any = new Error();
+      error.statusCode = 500;
+      error.message = "Please, update your browser";
+
+      return next(error);
+    }
+
+    return next();
+  },
+  (req, resp, next) => {
+    //resp.status(200);
+    //resp.statusMessage = "Ola mundo";
+    //resp.setHeader("Content-Type", "application/jason");
+    //resp.contentType = "application/jason";
+    //resp.send({ message: "Hello Word!" });
+    resp.json({
+      browser: req.userAgent(),
+      method: req.method,
+      url: req.href(),
+      path: req.path(),
+      query: req.query,
+    });
+    return next();
+  },
+]);
 
 server.listen(PORT, () =>
   console.log(`API is running on http://localhost:${PORT}`)
