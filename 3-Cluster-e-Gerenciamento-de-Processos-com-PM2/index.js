9d15407692d892a1cb49d0572e5ca4195af7bd3f
Bruno Silva Santana <ibrunoss@outlook.com>
2021-08-30 14:12:46 -0300

    Node Event Loop e Processos em Cluster

--- 3-Cluster-e-Gerenciamento-de-Processos-com-PM2/index.js
+++ 3-Cluster-e-Gerenciamento-de-Processos-com-PM2/index.js
@@ -5,9 +5,7 @@ const app = restify.createServer();
 app.use(restify.plugins.queryParser());
 
 app.get("/", (req, res, next) => {
-  for (let i = 0; i < 1e8; i++) {}
-
-  res.json({ pid: process.pid, echo: req.query });
+  setTimeout(() => res.json({ pid: process.pid, echo: req.query }), 500);
 });
 
 app.listen(4000, () => console.log("listening on port 4000"));
